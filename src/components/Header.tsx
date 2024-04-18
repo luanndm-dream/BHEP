c21e89a271fdcb63f1822c0f0b1606532fbee699
luanndm_dream <minhluannguyen2017@gmail.com>
2024-04-18 11:27:36 +0700

    update header

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, StatusBar } from 'react-native'
+import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { globalColor } from 'src/constants/color';
+
+
+interface HeaderProps {
+    headerTitle: String,
+    otherProp?: any
+    iconName?: any
+    onPressIcon?: () => void
+}
+const Header:React.FC<HeaderProps> = ({headerTitle,iconName,onPressIcon,otherProp}) => {
+  return (
+    <>
+
+    <SafeAreaView style={{flex: 0, backgroundColor: globalColor.primaryColor }} >
+    </SafeAreaView>
+    <View style={[styles.container]}>
+        <StatusBar backgroundColor={globalColor.primaryColor} barStyle={'light-content'}/>
+        <TouchableOpacity onPress={onPressIcon} style={styles.icon} >
+            <MaterialCommunityIcons name={iconName} size={40} color='white' />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>{headerTitle}</Text>
+    </View>
+    </>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    container: {
+        height: 67,
+        backgroundColor: globalColor.primaryColor,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+
+    },
+    icon: {
+       position: 'absolute',
+       left: 15
+    //    alignSelf: 'center'
+    },
+    headerTitle : {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center'
+    }
+})
---
+++ src/screens/HomeFlows/PartnerScreen.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
+import { Header } from '@/components'
+
+const PartnerScreen = () => {
+  return (
+    <>
+    <Header headerTitle='Đối tác' iconName='chevron-left' onPressIcon={()=>{}}/>
+    <ScrollView>
+    </ScrollView>
+    </>
+  )
+}
+
+export default PartnerScreen
+
+const styles = StyleSheet.create({})
