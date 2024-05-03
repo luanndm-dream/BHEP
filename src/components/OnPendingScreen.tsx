482f2dc9605dbc42933ed1f654c95d124a7bc09a
luanndm_dream <minhluannguyen2017@gmail.com>
2024-05-03 15:38:07 +0700

    gen key release

---
+++ src/components/OnPendingScreen.tsx
@@ -0,0 +1,27 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OnPendingScreen = () => {
+  return (
+    <View style={styles.outerContainer}>
+      <View style={styles.innerContainer}>
+        <Image source={require('../assets/image/pending.png')} style={{width: 100, height: 100}}/>
+        <Text>Tính năng đang được phát triển!</Text>
+      </View>
+    </View>
+  )
+}
+
+export default OnPendingScreen
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    alignItems:'center',    
+    justifyContent: 'center'
+  }
+})
