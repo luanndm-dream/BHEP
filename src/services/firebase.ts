f59b6f3945e51351ddecc76e5563ffed383bc56b
Nothing <113851050+luanndm-dream@users.noreply.github.com>
2024-12-03 00:39:59 +0700

    Hidden firebase key

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -8,14 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJGNumz2AIuBR1_p-hmHvlYPkj3_zzxTM",
-  authDomain: "bhep-demo.firebaseapp.com",
-  databaseURL: "https://bhep-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bhep-demo",
-  storageBucket: "bhep-demo.appspot.com",
-  messagingSenderId: "348701608516",
-  appId: "1:348701608516:web:95c2f13cd8e59509e18a11",
-  measurementId: "G-95E4PPDYEK"
+ **************************************
 };
 
 // Initialize Firebase
