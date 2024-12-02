ccad66c06f6bf58d4bf5a7fe3e3b2e0c903c26a9
Nothing <113851050+luanndm-dream@users.noreply.github.com>
2024-12-03 00:40:32 +0700

    Hidden firebase key

--- src/services/firebase-iot.ts
+++ src/services/firebase-iot.ts
@@ -7,14 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCUJOl5jkzMIO1KRxgJyEPtiipPNzgccEg",
-  authDomain: "bhep-iot.firebaseapp.com",
-  databaseURL: "https://bhep-iot-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bhep-iot",
-  storageBucket: "bhep-iot.appspot.com",
-  messagingSenderId: "46057334140",
-  appId: "1:46057334140:web:0005a64d92084ecae430f9",
-  measurementId: "G-FY45GCZ5GH"
+**************************************
 };
 
 // Initialize Firebase
